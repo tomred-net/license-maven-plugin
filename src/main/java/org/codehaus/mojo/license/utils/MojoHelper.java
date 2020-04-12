c9f09f7b33da6aafb3adc4db3b17a4f009174cb0
Bitbucket Pipeline <devops@wych.it>
2020-04-12 22:15:54 +1200

    Fix #358 NPE on site generation caused by null returned by deprecated MavenProject.getDependencyArtifacts()

--- src/main/java/org/codehaus/mojo/license/utils/MojoHelper.java
+++ src/main/java/org/codehaus/mojo/license/utils/MojoHelper.java
@@ -46,7 +46,7 @@ import java.util.Set;
  */
 public class MojoHelper
 {
-    private static final Logger LOG = LoggerFactory.getLogger ( MojoHelper.class );
+    private static final Logger LOG = LoggerFactory.getLogger( MojoHelper.class );
 
     /**
      * Add the directory as a resource of the given project.
@@ -58,8 +58,8 @@ public class MojoHelper
      */
     public static boolean addResourceDir( File dir, MavenProject project, String... includes )
     {
-        List<?> resources = project.getResources ();
-        return addResourceDir ( dir, project, resources, includes );
+        List<?> resources = project.getResources();
+        return addResourceDir( dir, project, resources, includes );
     }
 
     /**
@@ -73,21 +73,21 @@ public class MojoHelper
      */
     public static boolean addResourceDir( File dir, MavenProject project, List<?> resources, String... includes )
     {
-        String newresourceDir = dir.getAbsolutePath ();
+        String newresourceDir = dir.getAbsolutePath();
         boolean shouldAdd = true;
         for ( Object o : resources )
         {
             Resource r = (Resource) o;
-            if ( !r.getDirectory ().equals ( newresourceDir ) )
+            if ( !r.getDirectory().equals( newresourceDir ) )
             {
                 continue;
             }
 
             for ( String i : includes )
             {
-                if ( !r.getIncludes ().contains ( i ) )
+                if ( !r.getIncludes().contains( i ) )
                 {
-                    r.addInclude ( i );
+                    r.addInclude( i );
                 }
             }
             shouldAdd = false;
@@ -95,23 +95,23 @@ public class MojoHelper
         }
         if ( shouldAdd )
         {
-            Resource r = new Resource ();
-            r.setDirectory ( newresourceDir );
+            Resource r = new Resource();
+            r.setDirectory( newresourceDir );
             for ( String i : includes )
             {
-                if ( !r.getIncludes ().contains ( i ) )
+                if ( !r.getIncludes().contains( i ) )
                 {
-                    r.addInclude ( i );
+                    r.addInclude( i );
                 }
             }
-            project.addResource ( r );
+            project.addResource( r );
         }
         return shouldAdd;
     }
 
     public static Comparator<MavenProject> newMavenProjectComparator()
     {
-        return new Comparator<MavenProject> ()
+        return new Comparator<MavenProject>()
         {
             /**
              * {@inheritDoc}
@@ -119,9 +119,9 @@ public class MojoHelper
             public int compare( MavenProject o1, MavenProject o2 )
             {
 
-                String id1 = getArtifactId ( o1.getArtifact () );
-                String id2 = getArtifactId ( o2.getArtifact () );
-                return id1.compareTo ( id2 );
+                String id1 = getArtifactId( o1.getArtifact() );
+                String id2 = getArtifactId( o2.getArtifact() );
+                return id1.compareTo( id2 );
             }
         };
 
@@ -129,7 +129,7 @@ public class MojoHelper
 
     public static Comparator<MavenProject> newMavenProjectComparatorByName()
     {
-        return new Comparator<MavenProject> ()
+        return new Comparator<MavenProject>()
         {
             /**
              * {@inheritDoc}
@@ -137,9 +137,9 @@ public class MojoHelper
             public int compare( MavenProject o1, MavenProject o2 )
             {
 
-                String id1 = getProjectName ( o1 );
-                String id2 = getProjectName ( o2 );
-                return id1.compareToIgnoreCase ( id2 );
+                String id1 = getProjectName( o1 );
+                String id2 = getProjectName( o2 );
+                return id1.compareToIgnoreCase( id2 );
             }
         };
 
@@ -153,14 +153,14 @@ public class MojoHelper
 
     public static String convertTime( long value )
     {
-        return convert ( value, TIME_FACTORS, TIME_UNITES );
+        return convert( value, TIME_FACTORS, TIME_UNITES );
     }
 
     public static String convert( long value, double[] factors, String[] unites )
     {
-        long sign = value == 0 ? 1 : value / Math.abs ( value );
+        long sign = value == 0 ? 1 : value / Math.abs( value );
         int i = 0;
-        double tmp = Math.abs ( value );
+        double tmp = Math.abs( value );
         while (i < factors.length && i < unites.length && tmp > factors[i])
         {
             tmp = tmp / factors[i++];
@@ -168,7 +168,7 @@ public class MojoHelper
 
         tmp *= sign;
         String result;
-        result = MessageFormat.format ( "{0,number,0.###}{1}", tmp, unites[i] );
+        result = MessageFormat.format( "{0,number,0.###}{1}", tmp, unites[i] );
         return result;
     }
 
@@ -181,67 +181,67 @@ public class MojoHelper
      */
     public static URL getUrl( URL baseUrl, String suffix )
     {
-        String url = baseUrl.toString () + "/" + suffix;
+        String url = baseUrl.toString() + "/" + suffix;
         try
         {
-            return new URL ( url );
+            return new URL( url );
         }
         catch ( MalformedURLException ex )
         {
-            throw new IllegalArgumentException ( "could not obtain url " + url, ex );
+            throw new IllegalArgumentException( "could not obtain url " + url, ex );
         }
     }
 
     public static String getArtifactId( Artifact artifact )
     {
-        StringBuilder sb = new StringBuilder ();
-        sb.append ( artifact.getGroupId () );
-        sb.append ( "--" );
-        sb.append ( artifact.getArtifactId () );
-        sb.append ( "--" );
-        sb.append ( artifact.getVersion () );
-        return sb.toString ();
+        StringBuilder sb = new StringBuilder();
+        sb.append( artifact.getGroupId() );
+        sb.append( "--" );
+        sb.append( artifact.getArtifactId() );
+        sb.append( "--" );
+        sb.append( artifact.getVersion() );
+        return sb.toString();
     }
 
     public static String getArtifactName( MavenProject project )
     {
-        StringBuilder sb = new StringBuilder ();
-        if ( project.getName ().startsWith ( "Unnamed -" ) )
+        StringBuilder sb = new StringBuilder();
+        if ( project.getName().startsWith( "Unnamed -" ) )
         {
 
             // as in Maven 3, let's use the artifact id
-            sb.append ( project.getArtifactId () );
+            sb.append( project.getArtifactId() );
         }
         else
         {
-            sb.append ( project.getName () );
+            sb.append( project.getName() );
         }
-        sb.append ( " (" );
-        sb.append ( project.getGroupId () );
-        sb.append ( ":" );
-        sb.append ( project.getArtifactId () );
-        sb.append ( ":" );
-        sb.append ( project.getVersion () );
-        sb.append ( " - " );
-        String url = project.getUrl ();
-        sb.append ( url == null ? "no url defined" : url );
-        sb.append ( ")" );
+        sb.append( " (" );
+        sb.append( project.getGroupId() );
+        sb.append( ":" );
+        sb.append( project.getArtifactId() );
+        sb.append( ":" );
+        sb.append( project.getVersion() );
+        sb.append( " - " );
+        String url = project.getUrl();
+        sb.append( url == null ? "no url defined" : url );
+        sb.append( ")" );
 
-        return sb.toString ();
+        return sb.toString();
     }
 
     public static String getProjectName( MavenProject project )
     {
         String sb;
-        if ( project.getName ().startsWith ( "Unnamed" ) )
+        if ( project.getName().startsWith( "Unnamed" ) )
         {
 
             // as in Maven 3, let's use the artifact id
-            sb = project.getArtifactId ();
+            sb = project.getArtifactId();
         }
         else
         {
-            sb = project.getName ();
+            sb = project.getName();
         }
 
         return sb;
@@ -249,17 +249,16 @@ public class MojoHelper
 
     public static List<String> getParams( String params )
     {
-        String[] split = params == null ? new String[0] : params.split ( "," );
-        return Arrays.asList ( split );
+        String[] split = params == null ? new String[0] : params.split( "," );
+        return Arrays.asList( split );
     }
 
     /**
      * {@link MavenProject#getDependencyArtifacts()} is deprecated.
      *
      * <p>
-     * This method checks if the dependency artifacts is {@code null} and returns an
-     * empty {@code HashSet} to avoid the {@code NullPointerException}s caused by
-     * the {@link MavenProject#getDependencyArtifacts()} returning {@code null}.
+     * This method checks if the dependency artifacts is {@code null} and returns an empty {@code HashSet} to avoid the
+     * {@code NullPointerException}s caused by the {@link MavenProject#getDependencyArtifacts()} returning {@code null}.
      * </p>
      *
      * @param project the MavenProject to retrieve artifacts from
@@ -267,14 +266,14 @@ public class MojoHelper
      */
     public static Set<Artifact> getDependencyArtifacts( MavenProject project )
     {
-        if ( project == null || project.getDependencyArtifacts () == null )
+        if ( project == null || project.getDependencyArtifacts() == null )
         {
-            LOG.warn ( "" );
-            LOG.warn ( "Non-transitive dependencies cannot be found. " );
-            LOG.warn ( "" );
-            return new HashSet<Artifact> ();
+            LOG.warn( "" );
+            LOG.warn( "Non-transitive dependencies cannot be found. " );
+            LOG.warn( "" );
+            return new HashSet<Artifact>();
         }
-        return project.getDependencyArtifacts ();
+        return project.getDependencyArtifacts();
     }
 
 }
